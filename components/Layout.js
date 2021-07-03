ab80bc32a3ecced6b26d0ad42d4667cd721ed797
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-03 00:19:47 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,14 @@
-const Layout = () => {
-    return (  );
-}
- 
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="content">
+      <Navbar></Navbar>
+      {children}
+      <Footer></Footer>
+    </div>
+  );
+};
+
 export default Layout;
