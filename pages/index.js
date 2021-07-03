4e8ff2bbec84558d3b8a85491139e16e66f895a1
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-03 00:19:51 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Navbar></Navbar>
-      <h1>Home page</h1>
-      <p>
+      <h1 className={styles.title}>Home page</h1>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
         error, voluptatum iste ipsam aut sunt nobis dolore, voluptas repellendus
         ab eligendi? Ipsum harum deserunt architecto odit illo exercitationem
         itaque!
       </p>
-      <p>
+      <p className={styles.text}>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
         error, voluptatum iste ipsam aut sunt nobis dolore, voluptas repellendus
         ab eligendi? Ipsum harum deserunt architecto odit illo exercitationem
         itaque!
       </p>
       <Link href="/Listings">
-        <a>See Aditya Listing</a>
+        <a className={styles.btn}>See Aditya Listing</a>
       </Link>
-      <Footer></Footer>
     </div>
   );
 }
