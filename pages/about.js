335458adefde49c49370d545b59fbb0ca5c4c4c5
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-02 22:47:22 -0400

    Create about.js

---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
+        repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
+        commodi libero sunt magni quibusdam officia consectetur ipsam harum
+        voluptas?
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
+        repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
+        commodi libero sunt magni quibusdam officia consectetur ipsam harum
+        voluptas?
+      </p>
+    </div>
+  );
+};
+
+export default About;
