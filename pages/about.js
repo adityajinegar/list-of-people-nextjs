443b4e8019fbcb9d6a64796f50e0aa37cf27beb7
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-03 20:46:27 -0400

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,28 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
-        repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
-        commodi libero sunt magni quibusdam officia consectetur ipsam harum
-        voluptas?
-      </p>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
-        repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
-        commodi libero sunt magni quibusdam officia consectetur ipsam harum
-        voluptas?
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Aditya List | About</title>
+        <meta name="keywords" content="Aditya" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
+          repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
+          commodi libero sunt magni quibusdam officia consectetur ipsam harum
+          voluptas?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa odit
+          repellat debitis nemo id sed enim nostrum alias sunt! Necessitatibus
+          commodi libero sunt magni quibusdam officia consectetur ipsam harum
+          voluptas?
+        </p>
+      </div>
+    </>
   );
 };
 
