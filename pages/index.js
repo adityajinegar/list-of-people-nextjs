9239e14dfbf1e9f08dd8ae98e503bea1a5c8e0d5
Aditya Jinegar <aditya_jinegar@yahoo.com>
2021-07-03 20:46:35 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,23 +7,29 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home page</h1>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
-        error, voluptatum iste ipsam aut sunt nobis dolore, voluptas repellendus
-        ab eligendi? Ipsum harum deserunt architecto odit illo exercitationem
-        itaque!
-      </p>
-      <p className={styles.text}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
-        error, voluptatum iste ipsam aut sunt nobis dolore, voluptas repellendus
-        ab eligendi? Ipsum harum deserunt architecto odit illo exercitationem
-        itaque!
-      </p>
-      <Link href="/Listings">
-        <a className={styles.btn}>See Aditya Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Aditya List | Home</title>
+        <meta name="keywords" content="Aditya" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
+          error, voluptatum iste ipsam aut sunt nobis dolore, voluptas
+          repellendus ab eligendi? Ipsum harum deserunt architecto odit illo
+          exercitationem itaque!
+        </p>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat vel
+          error, voluptatum iste ipsam aut sunt nobis dolore, voluptas
+          repellendus ab eligendi? Ipsum harum deserunt architecto odit illo
+          exercitationem itaque!
+        </p>
+        <Link href="/Listings">
+          <a className={styles.btn}>See Aditya Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
